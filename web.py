60f848da04022009d4af2e7ffb0f2256a4863978
Sochamroun <267657641+Sochamroun@users.noreply.github.com>
2026-09-18 08:07:20 +0700

    Update web.py

--- web.py
+++ web.py
@@ -14,7 +14,7 @@ HTML = """
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>📥 ទាញយកវីដេអូ🇰🇭🤡</title>
+    <title>ទាញយកវីដេអូ</title>
     <style>
         * { box-sizing:border-box; }
         html, body { width:100%; min-height:100%; }
